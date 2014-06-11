fb21a83c4d11742dd3b9fd348ce176066b94976e
Krzysztof Szabelski <kszabelski@gmail.com>
2014-06-12 00:10:03 +0200

    Razor Helpers

--- AspNetViewsCodeReuseExamples/Controllers/HomeController.cs
+++ AspNetViewsCodeReuseExamples/Controllers/HomeController.cs
@@ -65,5 +65,19 @@ namespace AspNetViewsCodeReuseExamples.Controllers
 
             return model;
         }
+
+        public ActionResult RazorHelpers()
+        {
+            var model = new RazorHelpersViewModel
+            {
+                Users = new List<User> {
+                    new User {Name = "Barbara", Birthday = new DateTime(2001, 04, 28)},
+                    new User { Name = "Thomas", Surname = "Edison", Birthday = new DateTime(1847, 02, 11) },
+                    new User {Birthday = new DateTime(1987, 05, 25)}
+                }
+            };
+
+            return View(model);
+        }
     }
 }
---
+++ AspNetViewsCodeReuseExamples/ViewModels/RazorHelpersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AspNetViewsCodeReuseExamples.Models;
+
+namespace AspNetViewsCodeReuseExamples.ViewModels
+{
+    public class RazorHelpersViewModel
+    {
+        public List<User> Users { get; set; }
+    }
+}
